2e72b2bccd656e32b02314fb9e5d0bf3ae22c405
PD <168636679+chsny1pd@users.noreply.github.com>
2026-06-29 20:48:29 +0700

    setup mongodb connection

---
+++ lib/mongodb.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define MONGODB_URI");
+}
+
+let isConnected = false;
+
+export async function connectDB() {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true;
+
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    throw error;
+  }
+}
